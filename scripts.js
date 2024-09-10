9bd1c9b1a7374eb7c95cf5b235ecdc98c3d60901
shakeyiab <shakeyiabartell@gmail.com>
2024-09-10 16:04:04 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// loop through all numbers from 0-100/
+for(let i=1; i<=100; i++){
+    // Print each number
+    console.log(i);
+}
+ // find if number is divisable by 3
+for(i % 3===0)
+{
+    console.log("fizz" i)
+} 
+ // find if number is divisable by 5
+for( i % 5==-=0)
+{console.log("buzz")}
+ // find if number is divisable by 3 and 5
+for(i % 3) &&(i%5)
+{console.log(i)}
+
+// excersise 2 
+let n= 0;
+for(let n=1; n<=60; n++) {
+    for(let odd= 2; odd< i; odd++) {
+        if(n%odd ==0) {
+            console.log("is not a prime number"i);
+        }
+        console.log( "is a prime number" [i]);
+       n+=1;
+    }
+}
